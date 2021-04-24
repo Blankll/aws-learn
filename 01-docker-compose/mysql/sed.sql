ea9270f89be3f3e8b427d4a8e5de4cf90268c97e
seven <zilisheng1996@gmail.com>
2021-04-24 17:46:23 +0800

    add volume store db data

--- 01-docker-compose/mysql/sed.sql
+++ 01-docker-compose/mysql/sed.sql
@@ -1,8 +1,7 @@
 USE `shop`;
 
-INSERT INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES
-(1, '瓜子', 30, 10),
-(2, '汽水', 50, 4),
-(3, '凉茶', 44, 5),
-(4, '鸡爪', 88, 5),
-(6, '窝窝头', 55, 5.5);
+INSERT IGNORE INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES(1, '瓜子', 30, 10);
+INSERT IGNORE INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES(2, '汽水', 50, 4);
+INSERT IGNORE INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES(3, '凉茶', 44, 5);
+INSERT IGNORE INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES(4, '鸡爪', 88, 5);
+INSERT IGNORE INTO `goods`(`id`, `name`, `quantity`, `price`) VALUES(6, '窝窝头', 55, 5.5);
