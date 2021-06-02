e671f35a17fcf02ceb460884b95c032b3366af09
seven <zilisheng1996@gmail.com>
2021-06-02 23:30:27 +0800

    lambda copy s3 object from one bucket to another

--- 03-lambda/app.js
+++ 03-lambda/app.js
@@ -1,8 +1,37 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+const targetBucketName = 'seven-lambda-learn-target';
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    let body = 'Hello from Lambda!';
+    if (event.source === 'aws.events') {
+        console.log('triggerd by ScheduledEvent');
+        body = 'hello from ScheduledEvent';
+    } else if (event.Records && event.Records[0].eventSource === 'aws:s3'){
+        console.log('request s3 object', JSON.stringify(event.Records[0].s3.object));
+        try {
+            const exists = await s3.headObject({ Bucket: targetBucketName, Key: key}).promise().then(() => true).catch(err => err.code === 'NotFound' && false);
+            console.log('exists', JSON.stringify(exists));
+            if(!exists) {
+                const sourceBucketName = event.Records[0].s3.bucket.name;
+                const sourceKey = event.Records[0].s3.object.key;
+                const result = await s3.copyObject({
+                    Bucket: targetBucketName,
+                    CopySource: `${sourceBucketName}/${sourceKey}`,
+                    Key: sourceKey
+                }).promise();
+                console.log('upload to target bucket', JSON.stringify(result));
+            }
+            body = 'hello from s3 file copyed success!';
+        }catch(e) {
+            body = 'hello from s3 file copyed failed!';
+            console.error('document error cached', JSON.stringify(e));
+        }
+    }
+
+    return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(body),
     };
-    return response;
 };  
