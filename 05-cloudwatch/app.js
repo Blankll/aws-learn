f31d77e9a0c02bc806bcd279717361a247ecf205
seven <zilisheng1996@gmail.com>
2021-06-30 01:24:57 +0800

    add one cloudformation for lambda & custom metrics

---
+++ 05-cloudwatch/app.js
@@ -0,0 +1,33 @@
+const aws = require('aws-sdk');
+const cw = new aws.CloudWatch({apiVersion: '2010-08-01'});
+
+exports.handler = async (event) => {
+    let body = 'Hello from Lambda!';
+    let statusCode = 200;
+    console.log('event',JSON.stringify(event));
+    if (event.source === 'aws.events') {
+        body = 'hello from ScheduledEvent';
+        const params = {
+            Namespace: 'seven-metrics-namespace',
+            MetricData: [{
+                MetricName: 'seven-custom-metric',
+                Dimensions: [{
+                    Name: 'FunctionName',
+                    Value: 'seven-cloudwatch-learn-lambda'
+                }],
+                Unit: 'Count',
+                Value: 1
+            }]
+        };
+        const response = await cw.putMetricData(params).promise();
+        console.log('metrics update result:', JSON.stringify(response));
+    } else if (event.result === 'error') {
+        body = 'error request!';
+        statusCode = 400;
+    }
+    
+    return {
+        statusCode,
+        body: JSON.stringify(body),
+    };
+};
